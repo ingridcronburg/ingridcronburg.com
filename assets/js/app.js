100805abbd1a5d8258b25012d378d14316ca8338
Ingrid Cronburg <ingrid257@gmail.com>
2016-04-24 17:45:54 -0400

    a few updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,34 @@
-var $main = $(".main"),
+var $body = $("html"),
+    $main = $(".main"),
     $index = $(".index", $main),
     $gallery = $(".gallery", $main),
     $collage = $(".collage", $main),
     $collageList = $(".collage-list", $main),
     $sections = $index.add($gallery).add($collage).add($collageList);
-	
+
 var router = new Router({
-    before: function() { $sections.hide(); }, 
+    before: function() {
+      $body.removeClass('background');
+      $sections.hide();
+    },
     routes: {
-		  '/': function() { $index.show(); }, 
-      '/gallery/:name': function(name) { Gallery.load(name); $gallery.show(); }, 
-      '/collage': function() { $collageList.show(); },
-      '/collage/:name': function(name) { new Collage(name, $collage).show(); }	
+      '/': function() {
+        $body.addClass('background');
+        $index.show();
+      },
+
+      '/gallery/:name': function(name) {
+        Gallery.load(name);
+        $gallery.show();
+      },
+
+      '/collage': function() {
+        $collageList.show();
+      },
+
+      '/collage/:name': function(name) {
+        new Collage(name, $collage).show();
+      }
     }
 });
 
