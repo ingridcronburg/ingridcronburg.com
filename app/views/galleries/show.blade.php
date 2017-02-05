765bcf2cb03596861208eba46453e8a1031b6d50
Ingrid Cronburg <ingrid257@gmail.com>
2017-02-05 14:01:17 -0500

    gallery page updates

--- app/views/galleries/show.blade.php
+++ app/views/galleries/show.blade.php
@@ -2,10 +2,17 @@
 
 @section('content')
 <div class="page-gallery">
+  <div class="row text-center thumbnails">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    @foreach($gallery->images as $key => $image)
+      <img src="{{ $image->src }}" alt="{{ $image->title }}" data-index="{{ $key }}" class="{{ $key != 0 ?: 'selected' }} {{ $key <= 4 ?: 'hidden' }}" />
+    @endforeach
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </div>
   <div class="carousel slide" data-interval="false">
     <div class="carousel-inner" role="listbox">
     @foreach($gallery->images as $key => $image)
-      @if($key == 0)<div class="item active">@else<div class="item">@endif
+      <div data-index={{ $key }} class="item {{ $key != 0 ?: 'active' }}">
         <img src="{{ $image->src }}" alt="{{ $image->title }}" />
         <div class="caption">
           <p>{{ $image->title }}</p>
@@ -21,37 +28,55 @@
       <span class="sr-only">Next</span>
     </a>
   </div>
-  <div class="row text-center thumbnails">
-  @foreach($gallery->images as $key => $image)
-      <img src="{{ $image->src }}" alt="{{ $image->title }}" data-index="{{ $key }}" />
-  @endforeach
-  </div>
 </div>
 @endsection
 
 @section('scripts')
 @parent
 <script>
-  //$('.thumbnails img:first').addClass('selected');
+  function setThumbnailState(index) {
+    $('.thumbnails img.selected').removeClass('selected');
+    var thumbnail = $('.thumbnails img:eq(' + index + ')');
+    thumbnail.addClass('selected');
+
+    var clicked = thumbnail.data('index');
+    $('.thumbnails img').addClass('hidden');
+
+    $('.thumbnails img').each(function(i, val) {
+      var index = $(this).data('index');
+      var before_size = 2, after_size = 2;
+
+      if (clicked < 3) {
+        after_size = 4 - clicked;
+        before_size = clicked;
+      }
+
+      if (clicked > $('.thumbnails img').length - 4) {
+        before_size = clicked - ($('.thumbnails img').length - 5);
+        after_size = $('.thumbnails img').length - 1 - clicked;
+      }
+
+      if (index >= clicked - before_size && index <= clicked + after_size) {
+        $(this).removeClass('hidden');
+      }
+    });
+  }
 
   $('.thumbnails img').click(function() {
-    //$('.thumbnails img.selected').removeClass('selected');
-    //$(this).addClass('selected');
-    $('.carousel').carousel(parseInt($(this).attr('data-index')));
+    setThumbnailState($(this).data('index'));
+    $('.carousel').carousel(parseInt($(this).data('index')));
   });
 
-  $('.right.carousel-control').click(function() {
+  $('.right.carousel-control, .glyphicon-chevron-right').click(function() {
     $('.carousel').carousel('next');
-    //var $img = $('.active > img').attr('src');
-    //$('.thumbnails img.selected').removeClass('selected');
-    //$('.thumbnails img[src="'+$img+'"]').parent().next().children().addClass('selected');
   });
 
-  $('.left.carousel-control').click(function() {
+  $('.left.carousel-control, .glyphicon-chevron-left').click(function() {
     $('.carousel').carousel('prev');
-    //var $img = $('.active > img').attr('src');
-    //$('.thumbnails img.selected').removeClass('selected');
-    //$('.thumbnails img[src="'+$img+'"]').parent().prev().children().addClass('selected');
+  });
+
+  $('.carousel').on('slide.bs.carousel', function(event) {
+    setThumbnailState($(event.relatedTarget).data('index'));
   });
 </script>
 @endsection
