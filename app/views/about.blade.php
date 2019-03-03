044d5d8749d66b779fba8d47db1cd15d7edbf65a
Ingrid Cronburg <ingrid257@gmail.com>
2019-03-02 20:10:57 -0500

    Updating about page

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -11,25 +11,18 @@
         Hi, I'm Ingrid.
       </h1>
       <p class="vs">
-        I'm a software engineer in New York, NY.
+        I'm a software engineer in New York, New York.
       </p>
       <p class="vs">
-        I studied mathematics at Mount Holyoke College in South Hadley, MA (class of 2009).
-        I moved to New York in 2010 and spent 5 1/2 years as a budget analyst at the Metropolitan Opera.
-        Outside of work, I taught myself to code and built the very website you're looking at now!
-        I then enrolled in an immersive software engineering program at Fullstack Academy and graduated in fall 2016.
-      </p>
-      <p class="vs">
-        I'm always looking to expand my programming knowledge with new languages and technologies and you can often catch me roaming around Manhattan with my camera!
+        When I'm not coding, I like to knit, run, and explore the city with my camera.
       </p>
       <button type="button" class="btn btn-primary btn-lg mr20" data-toggle="modal" data-target=".contact-modal">
         Email Me
       </button>
       <div class="icons mt20">
-        <a href="https://www.facebook.com/ingrid.cronburg" target="_blank"><img src="/assets/img/facebook.png" /></a>
-        <a href="https://www.instagram.com/ingrid_cronburg" target="_blank"><img src="/assets/img/instagram.png" /></a>
-        <a href="https://github.com/ingridcronburg" target="_blank"><img src="/assets/img/github.png" /></a>
         <a href="https://linkedin.com/in/ingridcronburg" target="_blank"><img src="/assets/img/linkedin.png" /></a>
+        <a href="https://github.com/ingridcronburg" target="_blank"><img src="/assets/img/github.png" /></a>
+        <a href="https://www.instagram.com/ingrid_cronburg" target="_blank"><img src="/assets/img/instagram.png" /></a>
       </div>
     </div>
   </div>
